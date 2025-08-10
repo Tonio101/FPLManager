7e2a2ab2ad9490336989f5430ecc19f78c8b37ce
Antonio <antonio@antonio101.com>
2025-08-10 13:28:04 -0700

    Some minor refactoring

---
+++ heapnode.py
@@ -0,0 +1,15 @@
+import heapq
+
+
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+
+    def __repr__(self):
+        return f"Node value: {self.val}"
+
+    def __lt__(self, other):
+        if self.val.get_total_h2h_points() == other.val.get_total_h2h_points():
+            return other.val.get_total_points() < self.val.get_total_points()
+
+        return other.val.get_total_h2h_points() < self.val.get_total_h2h_points()
